c38be4d00d1bdfdf3955c7d9dccaa40fce4d011d
kmeng9025 <85904902+kmeng9025@users.noreply.github.com>
2025-07-15 14:26:28 -0400

    push

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ while True:
     frame = picam2.capture_array()
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     diff = cv2.absdiff(previousFrame, gray)
-    cv2.imshow("PiCamera2 Preview", frame)
 
     _, thresh = cv2.threshold(diff, 100, 255, cv2.THRESH_BINARY)
     dilated = cv2.dilate(thresh, None, iterations=20)
@@ -24,12 +23,13 @@ while True:
     if(len(contours) > 0):
         cv2.putText(frame, "Movement: True", (10, 20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2, cv2.LINE_AA)
     else:
-        cv2.putText(frame, "Movement: True", (10, 20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, "Movement: False", (10, 20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2, cv2.LINE_AA)
     for contour in contours:
         if cv2.contourArea(contour) < 100:
             continue
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     previousFrame = gray.copy()
