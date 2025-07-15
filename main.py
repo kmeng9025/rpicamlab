268d5fd6cb16be2e3300bae75eda01e741479985
kmeng9025 <85904902+kmeng9025@users.noreply.github.com>
2025-07-15 13:54:01 -0400

    push

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ picam2 = Picamera2()
 # picam2.preview_configuration.main.size = (640, 480)
 # picam2.preview_configuration.main.format = "RGB888"
 
-picam2.start()
-picam2.configure("preview")
-frame = picam2.capture_array()
-cv2.imshow("PiCamera2 Preview", frame)
-time.sleep(2)
+# picam2.start()
+# picam2.configure("preview")
+# frame = picam2.capture_array()
+# cv2.imshow("PiCamera2 Preview", frame)
+# time.sleep(2)
 
-while True:
-    frame = picam2.capture_array()
-    cv2.imshow("PiCamera2 Preview", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# while True:
+#     frame = picam2.capture_array()
+#     cv2.imshow("PiCamera2 Preview", frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
-# Clean up
-cv2.destroyAllWindows()
+# # Clean up
+# cv2.destroyAllWindows()
 picam2.close()
