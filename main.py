6d37ece46f29811f3881d6472a9ca897b7cc10e5
kmeng9025 <85904902+kmeng9025@users.noreply.github.com>
2025-07-15 13:59:06 -0400

    push

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 from picamera2 import Picamera2
 import cv2
 import time
-# try:
-    # Initialize camera
-picam2 = Picamera2()
-picam2.close()
-picam2 = Picamera2()
-picam2.preview_configuration.main.size = (640, 480)
-picam2.preview_configuration.main.format = "RGB888"
-
-picam2.start()
-picam2.configure("preview")
-frame = picam2.capture_array()
-cv2.imshow("PiCamera2 Preview", frame)
-time.sleep(2)
-
-while True:
+try:
+    picam2 = Picamera2()
+    picam2.preview_configuration.main.size = (640, 480)
+    picam2.preview_configuration.main.format = "RGB888"
+    picam2.configure("preview")
+    picam2.start()
     frame = picam2.capture_array()
     cv2.imshow("PiCamera2 Preview", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    time.sleep(2)
 
-# Clean up
-picam2.close()
-cv2.destroyAllWindows()
+    while True:
+        frame = picam2.capture_array()
+        cv2.imshow("PiCamera2 Preview", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# except:
-#     print("hi")
-#     cv2.destroyAllWindows()
-#     picam2.close()
+    # Clean up
+    picam2.close()
+    cv2.destroyAllWindows()
+
+except:
+    print("hi")
+    cv2.destroyAllWindows()
+    picam2.close()
