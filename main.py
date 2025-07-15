4cf432d24ef6270097f65abc2a7e3692f8edaae2
kmeng9025 <85904902+kmeng9025@users.noreply.github.com>
2025-07-15 17:59:08 -0400

    push

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ out = cv2.VideoWriter(str(start.timestamp()) + ".mp4", fourcc, 30, (1024, 768))
 while True:
     frame = picam2.capture_array()
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    diff = cv2.absdiff(previousFrame[80:670][300:650], gray[80:670][300:650])
+    diff = cv2.absdiff(previousFrame[80:670, 300:650], gray[80:670, 300:650])
     print(previousFrame)
-    print(gray[80:670][300:600].shape)
+    print(gray[80:670, 300:650].shape)
     print(diff.shape)
     movement += diff.sum()
     if(movement<0):
