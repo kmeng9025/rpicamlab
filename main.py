a8e80e617dbc41dead2ace975fc21f21ded3fa5b
kmeng9025 <85904902+kmeng9025@users.noreply.github.com>
2025-07-15 17:35:28 -0400

    push

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ while True:
         if (300 < x < 650) and (80 < y < 670):
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
             validMovement = True
-        elif(300 < x+w < 730) and (80 < y+h < 730):
+        elif(300 < x+w < 730) and (80 < y+h < 780):
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
             validMovement = True
         else:
