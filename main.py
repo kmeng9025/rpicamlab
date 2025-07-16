f4f88044906f6bd2cdcd451562c559f30c8853fe
kmeng9025 <85904902+kmeng9025@users.noreply.github.com>
2025-07-16 17:14:19 -0400

    push

--- main.py
+++ main.py
@@ -102,8 +102,8 @@ def main():
         # if(movement<0):
         #     print("INTEGER OVERFLOW ARGHHHHHHH")
         # print()
-        _, thresh = cv2.threshold(gray[60:720, 285:690], 50, 255, cv2.THRESH_BINARY_INV)
-        dilated = cv2.dilate(thresh, None, iterations=1)
+        _, thresh = cv2.threshold(gray[60:720, 285:690], 40, 255, cv2.THRESH_BINARY_INV)
+        dilated = cv2.dilate(thresh, None, iterations=0)
         contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         miceFound = False
         for contour in contours:
