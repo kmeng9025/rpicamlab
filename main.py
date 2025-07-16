a6bcf32fd616611a7a39e79a597b41072de3bf75
kmeng9025 <85904902+kmeng9025@users.noreply.github.com>
2025-07-16 16:59:11 -0400

    push

--- main.py
+++ main.py
@@ -158,6 +158,9 @@ def increase_brightness(img, value=30):
     v[v > lim] = 255
     v[v <= lim] += value
 
+    h[h > lim] = 255
+    h[h <= lim] += value
+
     final_hsv = cv2.merge((h, s, v))
     img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
     return img
