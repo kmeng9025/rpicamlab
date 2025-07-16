3d6eb06193c2ccd8473d1d637321fe4d29940ecd
kmeng9025 <85904902+kmeng9025@users.noreply.github.com>
2025-07-16 16:31:34 -0400

    push

--- main.py
+++ main.py
@@ -97,8 +97,8 @@ def main():
         # movement += diff.sum()
         # if(movement<0):
         #     print("INTEGER OVERFLOW ARGHHHHHHH")
-        
-        _, thresh = cv2.threshold(gray[60:720, 285:690], 35, 255, cv2.THRESH_BINARY_INV)
+
+        _, thresh = cv2.threshold(gray[60:720, 285:690], 30, 255, cv2.THRESH_BINARY_INV)
         dilated = cv2.dilate(thresh, None, iterations=1)
         contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         miceFound = False
