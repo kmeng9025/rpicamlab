109b9c913d165bd309add5e8427566e3a3ad0ae9
kmeng9025 <85904902+kmeng9025@users.noreply.github.com>
2025-08-21 14:16:10 -0400

    push

--- centralPi.py
+++ centralPi.py
@@ -74,14 +74,17 @@ def start_video(port):
 
 
 def display_video(port):
-    image = Image.fromarray(queue[port][-1])
-    image_tk = ImageTk.PhotoImage(image=image)
-    video_label.config(image=image_tk)
-    video_label.image = image_tk
-    queue[port].pop(-1)
-    queue[port] = []
+    try:
+        image = Image.fromarray(queue[port][-1])
+        image_tk = ImageTk.PhotoImage(image=image)
+        video_label.config(image=image_tk)
+        video_label.image = image_tk
+        queue[port].pop(-1)
+        queue[port] = []
+    except:
+        print("display video error")
     if(window == "v"):
-        root_window.after(10, partial(display_video, port))
+            root_window.after(10, partial(display_video, port))
 
 
 def listener():
