f3f27e824bea4d39a83f05f648c4315fe845406b
Kevin Meng <85904902+kmeng9025@users.noreply.github.com>
2025-09-19 18:35:26 -0400

    push

--- setUpCentral.sh
+++ setUpCentral.sh
@@ -4,14 +4,17 @@ chmod +x ./startCentralPi.sh
 # Get the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Make sure LXDE autostart folder exists
-AUTOSTART_DIR="/home/pi/.config/lxsession/LXDE-pi"
+# Detect current user’s home
+USER_HOME="$HOME"
+
+# Use LXDE autostart path
+AUTOSTART_DIR="$USER_HOME/.config/lxsession/LXDE"
 mkdir -p "$AUTOSTART_DIR"
 
 AUTOSTART_FILE="$AUTOSTART_DIR/autostart"
 
 # Remove any old entry for startCentralPi.sh
-sed -i '/startCentralPi.sh/d' "$AUTOSTART_FILE"
+sed -i '/startCentralPi.sh/d' "$AUTOSTART_FILE" 2>/dev/null || true
 
 # Add a line to start script in lxterminal from this directory
 echo "@lxterminal -e \"bash -c 'cd $SCRIPT_DIR && ./startCentralPi.sh; exec bash'\"" >> "$AUTOSTART_FILE"
